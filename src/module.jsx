ea4b9c7a06587dab010891452d56cacc962d8d66
Thomas-Spb <thomas-spb@list.ru>
2023-12-20 16:46:28 +0300

    first commit

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Pictha from './img/123.png'
+
+const Module = () => (
+    <>
+        <h1>Hello</h1>
+        <img src={Pictha} />
+    </>
+)
+
+export default Module
